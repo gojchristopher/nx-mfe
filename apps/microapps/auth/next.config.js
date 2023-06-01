aaad7d6b19b414bb212b7289ca864b5afabda0d9
John Christopher Go <go.jchristopher@gmail.com>
2023-06-01 10:34:36 +0800

    add mfe configuration

--- apps/microapps/auth/next.config.js
+++ apps/microapps/auth/next.config.js
@@ -2,15 +2,36 @@
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { withNx } = require('@nrwl/next/plugins/with-nx');
-
+const { NextFederationPlugin } = require('@module-federation/nextjs-mf');
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  distDir: 'build',
   nx: {
     // Set this to true if you would like to to use SVGR
     // See: https://github.com/gregberge/svgr
     svgr: false,
+  },
+  webpack(config, { isServer }) {
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: 'nexius-host',
+        filename: 'static/chunks/remoteEntry.js',
+        exposes: {
+          './login': './pages/index.tsx',
+        },
+        extraOptions: {
+          enableUrlLoaderFix: true,
+          automaticAsyncBoundary: true,
+        },
+      })
+    );
+    return config;
   },
 };
 
